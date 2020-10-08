fbfa7e49e684218cc358717c21f974b57b37ef4a
dyf <1339031399@qq.com>
2020-10-08 17:56:55 +0800

    app

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports={
+    // 配置postcss-pxtorem
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({
+            rootValue: 37.5,
+            unitPrecision: 5,
+            propList: ['*'],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0,
+            exclude: /node_modules/i
+          })
+        ]
+      }
+    }
+  },
+  //配置代理  http-proxy-middleware
+  devServer:{
+    open:true,
+    proxy:{
+        '/':{
+            target:'http:localhost:3000',
+            changeOrigin:true,
+           //  pathRewrite: {
+           //    '/api': '/'
+           //  }
+        }
+    }
+ }
+}
